bee13c3c87eee9da49d15848b2d5f0dacc2ff9fa
Jack Freund <jackkillian@tuta.com>
2026-07-23 19:41:53 -0500

    make sure various lanugages are setup for syntax highlighting

--- nvim/lua/syntax-highlight.lua
+++ nvim/lua/syntax-highlight.lua
@@ -1,8 +1,8 @@
 require("nvim-treesitter.config").setup({
-    ensure_installed = {"javascript", "python", "cpp"},
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-    },
+	ensure_installed = { "javascript", "typescript", "lua", "python", "cpp", "rust" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
 })
