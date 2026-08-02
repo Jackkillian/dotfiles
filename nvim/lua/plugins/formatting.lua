92ded376f8ff1caa273156fe8485c038637b5ebe
Jack Freund <jackkillian@tuta.com>
2026-08-01 20:51:35 -0500

    add support for typescript formatting

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -14,6 +14,8 @@ return {
 				html = { "prettierd", "prettier", stop_after_first = true },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 				css = { "prettierd", "prettier", stop_after_first = true },
 				scss = { "prettierd", "prettier", stop_after_first = true },
 				astro = { "prettierd", "prettier", stop_after_first = true },
